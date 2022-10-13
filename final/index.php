6a843904d3464e5787801c79c44ffb133a865f72
pvshenoy <91280014+pvshenoy@users.noreply.github.com>
2022-10-12 22:10:58 -0400

    Assignment 01

---
+++ final/index.php
@@ -0,0 +1,8 @@
+<?php include_once 'components/header.php'; ?>
+<?php $title = 'Assignment - 01'; ?>
+<h1>
+    <?php 
+        echo $title;
+    ?>
+</h1> 
+<?php include_once 'components/footer.php';?>
