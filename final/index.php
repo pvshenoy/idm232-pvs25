4ca4eae4c03dc67241ae63caa675b5314a60e69c
pvshenoy <91280014+pvshenoy@users.noreply.github.com>
2022-11-10 05:05:53 -0500

    alpha build

--- final/index.php
+++ final/index.php
@@ -1,8 +1,26 @@
 <?php include_once 'components/header.php'; ?>
-<?php $title = 'Assignment - 01'; ?>
-<h1>
-    <?php 
-        echo $title;
-    ?>
-</h1> 
+<header>
+        <h1>AROMA</h1>
+        <div class="menu">
+            <button>HOME</button>
+            <button>PERFUMES</button>
+            <button>NOTES</button>
+            <button>FORUM</button>
+            <button><img src="components/images/search.png" alt="search"></button>
+        </div>
+</header>
+<main>
+        <div class="hero_content">
+            <div class="text-left">
+                <h1>Welcome to the world of <span style="color: #94A5D7">fragrance</span></h1>
+                <div class="search_bar">
+                    <input type="text">
+                    <button>GO</button>
+                </div>
+            </div>
+            <div class="hero">
+                <img class="hero_img" src="components/images/hero_img.png" alt="fragrance-hero">
+            </div>
+        </div>
+</main>
 <?php include_once 'components/footer.php';?>
