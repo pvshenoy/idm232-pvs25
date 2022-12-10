8272b6350f7574927ba5a268b4da5485afce9614
pvshenoy <91280014+pvshenoy@users.noreply.github.com>
2022-12-09 23:59:39 -0500

    cleaning up files
    sweep sweep

--- public_html-Week7/example-page.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once __DIR__ . '/app.php';
-$page_title = 'Perfume';
-include_once __DIR__ . '/_components/header.php';
-?>
-
-MAIN CONTENT GOES HERE
-
-<?php include_once __DIR__ . '/_components/footer.php';
